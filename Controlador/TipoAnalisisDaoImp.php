8b63d41ed585aaaadb2acc235b7bddfcfcb47f37
mfrod <mfrod@10.31.10.95>
2018-06-29 14:19:40 -0400

    tipoAnalisisDao

---
+++ Controlador/TipoAnalisisDaoImp.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class TipoAnalisisDaoImp {
+    public static function ListarTodos(){
+        $analisis = new ArrayObject();
+        try{
+            $pdo = new clasePDO();
+            $stmt = $pdo->prepare("SELECT DISTINCT descripcion FROM tipoanalisis");
+            $stmt->execute();
+            
+            $rs = $stmt->fetchAll();
+            foreach ($rs as $value) {
+                $analisis->append($value["descripcion"]);
+            }
+            return $analisis;
+        } catch (Exception $ex) {
+                echo "Error al listar ".$ex->getMessage();
+        }
+    }
+}
