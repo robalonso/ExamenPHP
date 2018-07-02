985fd20b81940c75902a73c0e8390d8b04b28bb5
mfrod <mfrod@DESKTOP-IVPHNN1>
2018-07-02 00:59:38 -0400

    commit de muestra, recepcion, listado por procesar

--- Controlador/AnalisisMuestraDaoImp.php
+++ Controlador/AnalisisMuestraDaoImp.php
@@ -2,6 +2,7 @@
 
 include_once 'ClasePdo.php';
 include_once '../Modelo/AnalisisMuestra.php';
+
 class AnalisisMuestraDaoImp {
     
     public static function AgregarMuestra($dto){
@@ -37,4 +38,46 @@ class AnalisisMuestraDaoImp {
             echo "Error  al agreagar".$ex->getMessage();
         }
     }
+    
+    public  static function MostrarPorProcesar($noProcesado){
+        $lista = new ArrayObject();
+        try{
+            $pdo = new clasePDO();
+            $stmt = $pdo->prepare("SELECT * FROM analisismuestra WHERE procesado = ?");
+            $stmt->bindParam(1, $noProcesado);
+            
+            $stmt->execute();
+            $rs = $stmt->fetchAll();
+            
+            foreach ($rs as $muestra) {
+                if($muestra['procesado']==$noProcesado){
+                    $nuevaMuestra = new AnalisisMuestra();
+                    $nuevaMuestra->setIdAnalisis($muestra['idAnalisisMuestra']);
+                    $nuevaMuestra->setCantidad($muestra['cantidadMuestra']);
+                    $nuevaMuestra->setEmpresa_codigoEmpresa($muestra['empresa_codigoEmpresa']);
+                    $nuevaMuestra->setFechaRecepcion($muestra['fechaRecepcion']);
+                    $nuevaMuestra->setIdAnalisis($muestra['idAnalisisMuestra']);
+                    $nuevaMuestra->setParticular_codigoParticular($muestra['particular_codigoParticular']);
+                    $nuevaMuestra->setProcesado($muestra['procesado']);
+                    $nuevaMuestra->setTemperaturaMuestra($muestra['temperaturaMuestra']);
+                    $nuevaMuestra->setRutEmpleadoRecibe($muestra['rutEmpleadoRecibe']);
+                    $lista->append($nuevaMuestra);
+                    
+//                        private $idAnalisis;
+//    private $fechaRecepcion;
+//    private $temperaturaMuestra;
+//    private $cantidad;
+//    private $empresa_codigoEmpresa;
+//    private $particular_codigoParticular;
+//    private $procesado;
+//    private $rutEmpleadoRecibe;
+                }
+            }
+            return $lista;
+            
+        } catch (Exception $ex) {
+            echo "Error al listar, tonto conchetumare" . $ex->getMessage();
+        }
+    }
+    
 }
---
+++ Vista/v_MuestrasPorProcesar.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+
+    <body>
+        <h3>Listado de Muestras por Procesar</h3>
+        <div>
+            <table border="0">
+                <thead>
+                    <tr>
+                        <th>Código de Usuario</th>
+                        <th>Código de Muestra</th>
+                        <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                    include_once '../Modelo/AnalisisMuestra.php';
+                    include_once '../Controlador/AnalisisMuestraDaoImp.php';
+                    ?>
+                    <!--  Filtro de muestras no procesadas  -->
+                    <?php
+                    $procesado = 0;
+                    $listaMuestra = AnalisisMuestraDaoImp::MostrarPorProcesar($procesado);
+
+
+                    foreach ($listaMuestra as $muestra) {
+                        ?>
+                        <tr>
+
+                            <?php
+                            $texto = "";
+                            if ($muestra->getEmpresa_codigoEmpresa() == null) {
+                                if ($muestra->getParticular_codigoParticular() != null) {
+                                    $texto = $muestra->getParticular_codigoParticular();
+                                }
+                            }
+
+                            if ($muestra->getParticular_codigoParticular() == null) {
+                                if ($muestra->getEmpresa_codigoEmpresa() != null) {
+                                    $texto = $muestra->getEmpresa_codigoEmpresa();
+                                }
+                            }
+                            ?>
+
+                            <td> <?php echo $texto; ?> </td>
+
+                            <td> <?php echo $muestra->getIdAnalisis(); ?></td>
+
+                            <td>
+                                <form action="s_ProcesarMuestra" method="POST">
+                                    <input type="submit" value="Procesar" name="btnProcesar" />
+                                </form>
+                            </td>
+                        </tr>
+                    <?php }
+                    ?>
+
+                </tbody>
+            </table>
+
+        </div>
+    </body>
+</html>
